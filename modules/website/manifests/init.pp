5bf153cdc22db44bf1e44f4450e959f14bcac7cf
Gene Gotimer <gene.gotimer@coveros.com>
2015-04-30 21:15:52 -0400

    Puppet Exercise #2b, Templates

--- modules/website/manifests/init.pp
+++ modules/website/manifests/init.pp
@@ -7,4 +7,12 @@ class website {
     enable  => true,
     require => Package['apache2'],
   }
+  file { '/var/www/index.html':
+    ensure  => 'file',
+    content => template('website/index.html.erb'),
+    owner   => 'root',
+    group   => 'www-data',
+    mode    => '0640',
+    require => Package['apache2'],
+  }
 }
